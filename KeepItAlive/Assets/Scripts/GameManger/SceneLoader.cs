fa098f72a9328acf0597f3ce8cf184ff94fbb2b1
Alessio <alessio.bendoni91@gmail.com>
2020-04-19 00:43:38 +0200

    - Added fonts and Created assets for TextMeshPro - Added prefab for background - Added test scene for main menu - Added TextMeshPro - Changed Build target to Windows

---
+++ KeepItAlive/Assets/Scripts/GameManger/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void OnReloadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnReloadGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+}
