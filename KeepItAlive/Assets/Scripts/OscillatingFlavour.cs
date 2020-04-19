18a9a0a0584e71b87fad4efc0301b77f41dd0301
androbomb <alessandro.bombini.fi@gmail.com>
2020-04-19 10:11:31 +0200

    OF e PC SSandro

---
+++ KeepItAlive/Assets/Scripts/OscillatingFlavour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class OscillatingFlavour : MonoBehaviour
+{
+    #region Public fields
+    public Sprite[] neutrinoSprites;
+    #endregion
+
+    #region Private fields
+    private float changeTime = 1;
+    private int idx = 0;
+    #endregion
+
+    // Awake
+    private void Awake() {
+        neutrinoSprites = Resources.LoadAll<Sprite>("Neutrino_Sprites");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(flavourChange());
+
+        idx = Random.Range(0, neutrinoSprites.Length);
+
+        GetComponent<SpriteRenderer>().sprite = neutrinoSprites[ idx ];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // My personal functions
+    private IEnumerator flavourChange () 
+    {
+        while (true) {
+            
+            yield return new WaitForSeconds(changeTime); 
+
+            int index = Random.Range(0, neutrinoSprites.Length);
+
+            while (index == idx) {
+                index = Random.Range(0, neutrinoSprites.Length);
+            }
+
+            idx = index;
+
+            GetComponent<SpriteRenderer>().sprite = neutrinoSprites[ idx ];
+        }
+    }
+}
---
+++ KeepItAlive/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    #region Public fields
+    public Vector3 velocity;
+
+    /* public camera cam; */
+    #endregion
+    
+    #region Private fields
+    private Camera cam; 
+    private SpriteRenderer sprite;
+    private Vector2 screenBounds;
+    private float[] objectSize = {0,0,0};
+    private float[] minValues = {0,0,0};
+    private float[] maxValues = {0,0,0};
+    private Vector3 axis_position;
+    private string[] command = {"Horizontal", "Vertical", "Ano"};
+    #endregion
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        screenBounds = Camera.main.ScreenToWorldPoint(
+            new Vector3(
+                Screen.width, 
+                Screen.height, 
+                Camera.main.transform.position.z
+                )
+            );
+
+        
+        sprite = GetComponent<SpriteRenderer>();
+
+        objectSize[0] = sprite.bounds.size.x / 2;
+        objectSize[1] = sprite.bounds.size.y / 2;
+
+        minValues[0] = - screenBounds.x + objectSize[0];
+        minValues[1] = - screenBounds.y + objectSize[1];
+
+        maxValues[0] = screenBounds.x - objectSize[0]; 
+        maxValues[1] = screenBounds.y - objectSize[1];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        axis_position = transform.position; 
+
+        for (int i=0; i < 3-1; i++) { 
+
+            axis_position[i] += Input.GetAxis(command[i]) * velocity[i] * Time.deltaTime;
+             
+        } 
+
+        transform.position = axis_position;
+
+    }
+
+    
+    void LateUpdate()
+    {
+        Vector3 objectPosition = transform.position;
+
+        objectPosition.x = Mathf.Clamp(objectPosition.x, minValues[0], maxValues[0]);
+        objectPosition.y = Mathf.Clamp(objectPosition.y, minValues[1], maxValues[1]);
+
+        transform.position = objectPosition;
+    }
+}
