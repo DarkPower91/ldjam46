dbdaadf17bceb40d973fc1049f8c40b992359f60
Alessio <alessio.bendoni91@gmail.com>
2020-04-20 12:36:13 +0200

    - Addded You travelled - Added Hide with flow component

---
+++ KeepItAlive/Assets/Scripts/GameManger/HideWithFlow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideWithFlow : MonoBehaviour
+{
+    public GameState HiddenInFlowState = GameState.MainMenu;
+
+    void Start()
+    {
+        FlowManager.OnGameStateChanged += OnFlowStateChanged;
+    }
+
+    void OnDestroy()
+    {
+        FlowManager.OnGameStateChanged -= OnFlowStateChanged;
+    }
+
+    private void OnFlowStateChanged(GameState newState)
+    {
+        if(newState == HiddenInFlowState)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
