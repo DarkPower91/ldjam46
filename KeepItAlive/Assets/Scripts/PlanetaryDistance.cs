1da1622379c1f55cfb063a3bdd2172152aa1f655
androbomb <alessandro.bombini.fi@gmail.com>
2020-04-20 22:43:35 +0200

    PlanetPopUp definitive

---
+++ KeepItAlive/Assets/Scripts/PlanetaryDistance.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // IMPORTANT!!!!!!!!
+
+public class PlanetaryDistance : MonoBehaviour
+{
+    [System.Serializable]
+    public class PlanetDefiner 
+    {
+        public bool reached;
+        public string planetName;
+        public float planetDistance;
+    }
+
+    #region Public fields
+    [Readonly]
+    public float neutrinoPercurredDistance = 0.0f;
+    public ComputePercurredDistance playerDistanceComponent = null;
+    public string extraTextBefore= "";
+    public string extraTextAfter = "";
+    public float timeExposedString = 3.0f;
+    public PlanetDefiner[] planets;
+    #endregion 
+
+    private Text scoreText;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerDistanceComponent != null) 
+        {
+            if (FlowManager.GetGameState() == GameState.InGame) 
+            {
+                foreach(PlanetDefiner planet in planets)
+                {
+                    if (!planet.reached)
+                    {
+                        if (playerDistanceComponent.neutrinoPercurredDistance >= planet.planetDistance)
+                        {
+                            if (planet.planetName == "Earth") 
+                            {
+                                scoreText.text = extraTextBefore + " " + planet.planetName + "! Say hello to Gino! " + extraTextAfter;
+                            }
+                            else if (planet.planetName == "Pluto") 
+                            {
+                                scoreText.text = extraTextBefore + " " + planet.planetName + "! actually, it's not a planet, but he's cute! " + extraTextAfter;
+                            }
+                            else if (planet.planetName == "Outside") 
+                            {
+                                scoreText.text = "You are now leaving the Solar system!";
+                            }
+                            else
+                            {
+                                scoreText.text = extraTextBefore + " " + planet.planetName + "! " + extraTextAfter;
+                            }
+                            planet.reached = true;
+                            StartCoroutine(awaitFunc());
+                        }
+                    }
+                } 
+            }
+        }
+    }
+
+    IEnumerator awaitFunc() 
+    {
+        yield return new WaitForSeconds(timeExposedString);
+
+        scoreText.text = "";
+    }
+}
--- KeepItAlive/Assets/Scripts/PlanetesPopUp.cs
+++ KeepItAlive/Assets/Scripts/PlanetesPopUp.cs
@@ -11,7 +11,6 @@ public class PlanetesPopUp : MonoBehaviour
     public ComputePercurredDistance playerDistanceComponent = null;
     public string extraTextBefore= "";
     public string extraTextAfter = "";
-    public float timeShowed = 1.0f;
     #endregion
 
     #region Private fields
@@ -20,6 +19,11 @@ public class PlanetesPopUp : MonoBehaviour
     private float venusDist = 0.723f;
     private float earthDist = 1.0f;
     private float marsDist = 1.52f;
+    private float jupiterDist = 5.20f;
+    private float saturnDist = 9.60f;
+    private float uranusDist = 19.2f;
+
+
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -30,22 +34,40 @@ public class PlanetesPopUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (playerDistanceComponent != null) 
         {
             if (FlowManager.GetGameState() == GameState.InGame) 
             {
                 float distPerc = playerDistanceComponent.neutrinoPercurredDistance;
-                if ( distPerc > mercuryDist && distPerc < mercuryDist + timeShowed ) 
+
+                if ( distPerc > mercuryDist && distPerc < venusDist ) 
                 {
                     scoreText.text = extraTextBefore + " Mercury! " + extraTextAfter;
                 }
-                else if (distPerc > venusDist && distPerc < venusDist + timeShowed ) 
+                else if (distPerc > venusDist && distPerc < earthDist ) 
                 {
                     scoreText.text = extraTextBefore + " Venus! " + extraTextAfter;
                 }
-                else if (distPerc > earthDist && distPerc < earthDist + timeShowed ) 
+                else if (distPerc > earthDist && distPerc < marsDist ) 
                 {
                     scoreText.text = extraTextBefore + " Earth! Say hello to Gino!" + extraTextAfter;
+                }
+                else if (distPerc > marsDist && distPerc < jupiterDist )
+                {
+                    scoreText.text = extraTextBefore + " Mars! " + extraTextAfter;
+                }
+                else if (distPerc > jupiterDist && distPerc < saturnDist )
+                {
+                    scoreText.text = extraTextBefore + " Jupietr! " + extraTextAfter;
+                }
+                else if (distPerc > saturnDist && distPerc < uranusDist )
+                {
+                    scoreText.text = extraTextBefore + " Uranus! " + extraTextAfter;
+                }
+                else if (distPerc> uranusDist)
+                {
+                    scoreText.text = "You exit the solar system!";
                 }
             }
         }
