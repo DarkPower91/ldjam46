7889131d717c09ac3eb62871a7591080792e71a3
Alessio <alessio.bendoni91@gmail.com>
2020-04-20 18:57:17 +0200

    - Added Dex to the gameloop

---
+++ KeepItAlive/Assets/Scripts/GameManger/GameplayUIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayUIManager : MonoBehaviour
+{
+    public GameObject m_GameOverCanvas = null;
+    public GameObject m_DexCanvas = null;
+
+    private GameObject m_ActiveCanvas = null;
+
+    public void ActivateDex()
+    {
+        m_ActiveCanvas.SetActive(false);
+        m_DexCanvas.SetActive(true);
+        m_ActiveCanvas = m_DexCanvas;
+    }
+
+    public void ActivateGameOver()
+    {
+        m_ActiveCanvas.SetActive(false);
+        m_GameOverCanvas.SetActive(true);
+        m_ActiveCanvas = m_GameOverCanvas;
+    }
+
+    void Start()
+    {
+        m_ActiveCanvas = m_GameOverCanvas;
+    }
+}
