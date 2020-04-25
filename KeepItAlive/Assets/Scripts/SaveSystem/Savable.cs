d1b2a7ffeea7d294e3a2e947bed28b145fe192c6
Alessio <alessio.bendoni91@gmail.com>
2020-04-26 01:26:25 +0200

    - Added save and load events - Added savable abstract class to be implemented in later changes

---
+++ KeepItAlive/Assets/Scripts/SaveSystem/Savable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Savable : MonoBehaviour
+{
+    private void Start()
+    {
+        SaveEvents.SaveInitiated += OnSave;
+        SaveEvents.LoadInitiated += OnLoad;
+    }
+
+    private void OnDestroy() 
+    {
+        SaveEvents.SaveInitiated -= OnSave;
+        SaveEvents.LoadInitiated -= OnLoad;   
+    }
+
+    protected abstract void OnSave();
+    protected abstract void OnLoad();
+}
---
+++ KeepItAlive/Assets/Scripts/SaveSystem/SaveEvents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+public class SaveEvents
+{
+    public static Action SaveInitiated = null;
+    public static Action LoadInitiated = null;
+    public static Action ClearSaveInitiated = null;
+
+    public static void OnSaveInitiated()
+    {
+        SaveInitiated?.Invoke();
+    }
+
+    public static void OnLoadInitiated()
+    {
+        LoadInitiated?.Invoke();
+    }
+
+    public static void OnClearSaveInitiated()
+    {
+        ClearSaveInitiated?.Invoke();
+    }
+}
