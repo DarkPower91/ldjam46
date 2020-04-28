90239783f0f1b979e2b9bd57cefe565ceddff4dd
androbomb <alessandro.bombini.fi@gmail.com>
2020-04-28 14:50:44 +0200

    added localizaion tag

--- KeepItAlive/Assets/Scripts/GameManger/ManagersLoader.cs
+++ KeepItAlive/Assets/Scripts/GameManger/ManagersLoader.cs
@@ -6,11 +6,13 @@ public class ManagersLoader : MonoBehaviour
 {
     #region Serialized fields
     [SerializeField]
-    private  GameObject m_SaveDatarepository = null;
+    private  GameObject m_SaveDataRepository = null;
     [SerializeField]
     private  GameObject m_FlowManager = null;
     [SerializeField]
     private GameObject m_MusicManager = null;
+    [SerializeField]
+    private GameObject m_LocalizationData = null;
     #endregion
 
     // Start is called before the first frame update
@@ -18,7 +20,7 @@ public class ManagersLoader : MonoBehaviour
     {
         if(!GameObject.FindGameObjectWithTag("SaveData"))
         {
-            GameObject saveData = Instantiate(m_SaveDatarepository, transform.position, Quaternion.identity) as GameObject;
+            GameObject saveData = Instantiate(m_SaveDataRepository, transform.position, Quaternion.identity) as GameObject;
             DontDestroyOnLoad(saveData);
         }
 
@@ -32,6 +34,12 @@ public class ManagersLoader : MonoBehaviour
         {
             GameObject flowManager = Instantiate(m_FlowManager, transform.position, Quaternion.identity) as GameObject;
             DontDestroyOnLoad(flowManager);
+        }
+
+        if(!GameObject.FindGameObjectWithTag("LocalizationData") && m_LocalizationData != null)
+        {
+            GameObject localizationData = Instantiate(m_LocalizationData, transform.position, Quaternion.identity) as GameObject;
+            DontDestroyOnLoad(localizationData);
         }
     }
 }
