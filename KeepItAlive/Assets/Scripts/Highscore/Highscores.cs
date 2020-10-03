ebd6adb9173227002de3253636d16c1155408df0
Alessio <alessio.bendoni91@gmail.com>
2020-10-03 16:43:37 +0200

    - Added High score and leaderboard

---
+++ KeepItAlive/Assets/Scripts/Highscore/Highscores.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highscores : MonoBehaviour {
+
+	const string privateCode = "9neaXpcCLkOtXdYOhchXSQNbTc82Ba8E6tN1wFCJXtXg";
+	const string publicCode = "5f77586deb371809c45e43fe";
+	const string webURL = "http://dreamlo.com/lb/";
+
+	DisplayHighscores highscoreDisplay;
+	public Highscore[] highscoresList;
+	static Highscores instance;
+	
+	void Awake() {
+		highscoreDisplay = GetComponent<DisplayHighscores> ();
+		instance = this;
+	}
+
+	public static void AddNewHighscore(string username, float score)
+	{
+		int scoreInt = (int) (score * 1000000);
+		AddNewHighscore(username, scoreInt);
+	}
+
+	private static void AddNewHighscore(string username, int score) {
+		instance.StartCoroutine(instance.UploadNewHighscore(username,score));
+	}
+
+	IEnumerator UploadNewHighscore(string username, int score) {
+		WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+		yield return www;
+
+		if (string.IsNullOrEmpty(www.error)) {
+			print ("Upload Successful");
+			DownloadHighscores();
+		}
+		else {
+			print ("Error uploading: " + www.error);
+		}
+	}
+
+	public void DownloadHighscores() {
+		StartCoroutine("DownloadHighscoresFromDatabase");
+	}
+
+	IEnumerator DownloadHighscoresFromDatabase() {
+		WWW www = new WWW(webURL + publicCode + "/pipe/");
+		yield return www;
+		
+		if (string.IsNullOrEmpty (www.error)) {
+			FormatHighscores (www.text);
+			highscoreDisplay.OnHighscoresDownloaded(highscoresList);
+		}
+		else {
+			print ("Error Downloading: " + www.error);
+		}
+	}
+
+	void FormatHighscores(string textStream) {
+		string[] entries = textStream.Split(new char[] {'\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+		highscoresList = new Highscore[entries.Length];
+
+		for (int i = 0; i <entries.Length; i ++) {
+			string[] entryInfo = entries[i].Split(new char[] {'|'});
+			string username = entryInfo[0];
+			int score = int.Parse(entryInfo[1]);
+			float scoreFloat = (float)score /1000000;
+			highscoresList[i] = new Highscore(username,scoreFloat);
+		}
+	}
+
+}
+
+public struct Highscore {
+	public string username;
+	public float score;
+
+	public Highscore(string _username, float _score) {
+		username = _username;
+		score = _score;
+	}
+
+}
---
+++ KeepItAlive/Assets/Scripts/Highscore/SubmitWithButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+ 
+[DisallowMultipleComponent]
+[RequireComponent(typeof(TMP_InputField))]
+public class SubmitWithButton : MonoBehaviour
+{
+    public ComputePercurredDistance playerDistanceComponent = null;
+    public GameObject SubmitPanel = null;
+
+    public string submitKey = "Submit";
+    public bool trimWhitespace = true;
+    //Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+    //Apropriate when initializing fields.
+    void Start() {
+        _inputField = GetComponent<TMP_InputField>();
+        _inputField.onEndEdit.AddListener(fieldValue => {
+            if (trimWhitespace)
+                _inputField.text = fieldValue = fieldValue.Trim();
+            if (Input.GetButton(submitKey))
+                validateAndSubmit(fieldValue);
+        });
+    }
+    TMP_InputField _inputField;
+
+    bool isInvalid(string fieldValue) {
+        // change to the validation you want
+        return string.IsNullOrEmpty(fieldValue);
+    }
+    void validateAndSubmit(string fieldValue) {
+        if (isInvalid(fieldValue))
+            return;
+        Highscores.AddNewHighscore(fieldValue, playerDistanceComponent.neutrinoPercurredDistance);
+        SubmitPanel?.SetActive(false);
+
+        // change to whatever you want to run when user submits
+       // doSomething(_inputField); // or doSomething(fieldValue);
+    }
+    // to be called from a submit button onClick event
+    public void validateAndSubmit() {
+        validateAndSubmit(_inputField.text);
+    }
+}
